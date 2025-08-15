3beeb7af8ad62fb19b2b4c1d2b0ad2b05791c783
Ravi Purushotma <rpurushotma@gmail.com>
2025-08-15 14:43:39 -0700

    Fix Vite config to resolve syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/main.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
-  }
-});
+export default defineConfig({});
